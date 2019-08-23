cf817dad9b66dececd24d06b862cfae4bcb6432d
Derek Cross <derekcross@Leos-MacBook-Pro.local>
2019-08-23 17:43:19 -0400

    our first commit and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports =  {
+    "mode": "development",
+    "entry": "client/index.js",
+    "output": {
+        "path": __dirname+'/public',
+        "filename": "[name].[chunkhash:8].js"
+    },
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": "eslint-loader"
+            },
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            },
+            {
+                "test": /\.scss$/,
+                "use": [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+}
