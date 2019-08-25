6646f654c8d30288b07e2b3ecc520121093f5dbf
Gareth Leake <gfleake@gmail.com>
2019-08-25 15:18:12 -0400

    created UserHomePage to display on succesful login. Placed googleMap component on this UserHomePage. Presently trying to write a function that lets me access user location, and then store that information to be used as a new marker, as well as sending that information to the database. However, because of the asynchronous function getCurrentPosition, I have to write this as a promise. for some reason, react or eslint (unsure which one) does not recognize the promise. Need to figure out how to use promises inside react.

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import WrappedMapWithMarker from './googleMap.jsx';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import UserHomePage from './UserHomePage.jsx';
 
 
 class App extends Component {
 
     render(){
         return(
-            <div className={'mapContainer'}>
-                <div className='googleMap'>
-                    <WrappedMapWithMarker 
-                    googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyDfGA9uSqnTuRHc7V6c0tlEsyZaEBZKFeA&v=3.exp&libraries=geometry,drawing,places'}
-                    loadingElement={<div style={ {height: '100%'} }/> }
-                    containerElement={<div style={ {height: '100%'} }/> }
-                    mapElement={<div style={ {height: '100%'} }/> } 
-                    />
-                </div>
+            <div>
+            PLACEHOLDER TEXT FOR LOGIN AND SIGNUP
+            {/* login / signup goes here */}
+            {/* on succesful login, redirect to the home component */}
+                <UserHomePage />
             </div>
         );
     }
--- client/googleMap.jsx
+++ client/googleMap.jsx
@@ -1,13 +1,13 @@
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
 const MapWithAMarker = withScriptjs(withGoogleMap(props => 
   <GoogleMap
     defaultZoom={12}
     defaultCenter={{ lat: 40.719405, lng: -74.001824 }}
     >
-
       <Marker
         position={{ lat: 40.719405, lng: -74.001824 }}
       />
