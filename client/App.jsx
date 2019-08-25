772b8a0f657a3d6b2e7539ade3150488919b147a
Derek Cross <derekcross@Leos-MacBook-Pro.local>
2019-08-25 16:36:51 -0400

    saving changes derek branch

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,25 @@ import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'
 
   class App extends React.Component {
+
+    constructor(){
+        super();
+      
+        this.handlLogin = this.handleLogin.bind(this)
+        this.handleSignup = this.handleSignup.bind(this)
+    }
+
+    componentDidMount(){
+
+    }
+
+    handleLogin(){
+
+    }
+    handleSignup(){
+
+    }
+
     render() {
       return (
         <div className="router">
