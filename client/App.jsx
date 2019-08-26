c14e307b6ab39888e30abd41ea59c37b5b9c2e5b
Derek Cross <derekcross@Leos-MacBook-Pro.local>
2019-08-26 11:09:48 -0400

    frontend integration

--- client/App.jsx
+++ client/App.jsx
@@ -1,47 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Signup from './components/Signup.jsx'
-import Login from './components/Login.jsx'
+import Signup from './components/Signup.jsx';
+import Login from './components/Login.jsx';
+import UserHomePage from './components/UserHomePage.jsx';
 
-  class App extends React.Component {
-
-    constructor(){
-        super();
-      
-        this.handlLogin = this.handleLogin.bind(this)
-        this.handleSignup = this.handleSignup.bind(this)
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            data: [],
+            login: false
+        };
+        this.handleSubmit = this.handleSubmit.bind(this)
+        
     }
-
-    componentDidMount(){
-
+    
+    handleSubmit(login) {
+        console.log('this.state.data here', this.state);
+        console.log('login', login);
+        
+        this.setState({data: [...this.state.data]});
     }
-
-    handleLogin(){
-
-    }
-    handleSignup(){
-
-    }
-
     render() {
-      return (
+      const { data } = this.state;
+    //   const login = false
+    console.log('data here', data);
+    
+     Object.values(data).map(val=>{
+         if(val==='derek')  this.setState({data:data, login:true})
+         else this.setState({data:data, login:false})
+     })
+      
+      console.log('data here in perfect fetch', data); 
+      console.log('login here', this.state.login);
+       
+      
+
+      if(this.state.login===false) return (
         <div className="router">
         <main>
           <Switch>
-            <Route exact path="/"
-              component={
-                () => <Login />
-              }
-            />
-            <Route exact path="/Signup"
-              component={
-                () => <Signup  />
-              }
-            />
+            <Route exact path="/" component={()=><Login  handleSubmit={this.handleSubmit} /> } />
+            <Route exact path="/Signup"component={()=><Signup />}/>
           </Switch>
         </main>
       </div>
       );
+      else return (
+        <div className="router">
+        <main>
+          <Switch>
+            <Route exact path="/" component={()=><UserHomePage /> } />
+          </Switch>
+        </main>
+      </div>
+      )
     }
   }
   
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,23 +1,57 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom';
 
-class Login extends React.Component {
-    render() {
-      return (
-        <div>
-        
-        <div id="container-login" align="center">
-          <form>
-          <h2>Login</h2>
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Go!</button>
-            <br /><br />To register go to <Link to={`/Signup`}>Signup page</Link>
-          </form>
-        </div>
-        </div>
-      );
-    }
+class Login extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      name: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this)
+   
+
+  }
+  handleChange(event){
+      const { name, value } = event.target;
+
+      this.setState({
+          [name] : value
+      });
   }
 
-  export default Login
+  onFormSubmit(event){
+      event.preventDefault();
+
+      this.props.handleSubmit(this.state);
+      this.setState(this.initialState);
+
+  }
+
+  render() {
+    const { name,  password } = this.state; 
+    console.log('name here', name);
+    console.log(this.state);
+    
+    return (
+      <div>
+      <div id="container-login" align="center">
+        <form onSubmit={()=>{
+          this.props.handleSubmit(this.state);
+          this.setState(this.initialState);
+        }}>
+
+        <h2>Login</h2>
+          <input type="text" name="name" value={name} onChange={this.handleChange} placeholder="Email" />
+          <input type="password" name="password" value={password} onChange={this.handleChange}  placeholder="Password" />
+          <button type="submit">Go!</button>
+          <br /><br />To register go to <Link to={`/Signup`}>Signup page</Link>
+        </form>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Login
