5b63acb98fc95f7fc59dc449146cb558feb179b3
Yu Jin Kang <yujinkang@PK-2.local>
2019-08-27 20:51:39 -0400

    login component initial commit

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-
+    const url = 'http://localhost/8080'; 
+    // State hooks to update the username and password 
+    const [ email, setEmail ] = useState(''); 
+    const [ password, setPassword ] = useState(''); 
+    // Handler to update the email state
+    const emailHandler = (e) => {
+        const userEmail = e.target.value.trim(); 
+        setEmail(userEmail); 
+    }; 
+    // Handler to update the password state
+    const passwordHandler = (e) => {
+        const userPassword = e.target.value.trim(); 
+        setPassword(userPassword); 
+    }; 
+    // Handler to send a post request 
+    const userVerifyHandler = () => {
+        // Post request to server 
+        fetch(url, {
+          method: 'POST', 
+          mode: 'cors', 
+          headers: {
+            'Content-Type': 'application/json', 
+          }, 
+          body: {
+            Email: email, 
+            Password: password,
+          }
+        })
+        // Server should send a boolean to determine whether
+        // the credentials are correct
+        .then(res => res.text())
+        .then(res => {
+            // Check to see the server response
+            if (res === true) {
+              console.log('It worked')
+              // Render the homepage 
+            } else {
+              alert('Your credentials are incorrect. Try again.')
+            }
+        }); 
+    }; 
+    
+    return (
+      <div>
+        <label>Email:</label><input onChange={(e) => {emailHandler(e)}}></input>
+        <label>Password:</label><input onChange={(e) => {passwordHandler(e)}}></input>
+        <button onClick={() => {userVerifyHandler()}}>Submit</button>
+      </div>
+    )
 }; 
 
 export default Login; 
