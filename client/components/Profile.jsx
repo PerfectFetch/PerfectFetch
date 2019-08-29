4e842028afbdb06b5f799658fa2ecb1a4d769d8f
Ruthba Anam <ruthbaanam@gmail.com>
2019-08-29 12:18:52 -0400

    imported username and zipcode from signup into profile

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 import { username, zipcode } from './Login.jsx'; 
+import { userAndZip } from './Signup.jsx';
+
+const modifyUserAndZip = () => {
+  // if user wants to edit their username and/or zip
+  console.log(userAndZip.username)
+  console.log(userAndZip.zipcode)
+
+}
 
 const Profile = () => {
   // Set our username, bio, tags state
@@ -54,8 +62,8 @@ const Profile = () => {
   return (
     <div>
       <div id="usernameContainer">
-        <label>Username: </label>
-        <input
+        <label>Username: {userAndZip.username}</label>
+        {/* <input
           name='username'
           type='text'
           // style={{ margin: 8 }}
@@ -63,7 +71,12 @@ const Profile = () => {
           onChange={e => setUsername(e.target.value.trim())}
           value={username}
           variant="outlined"
-          required />
+          required /> */}
+        <br />
+      </div>
+
+      <div id="zipCodeContainer">
+        <label>Zipcode: {userAndZip.zipcode}</label>
         <br />
       </div>
 
@@ -80,20 +93,21 @@ const Profile = () => {
           />
       </div>
       <h4>Select pair programming interests:</h4>
-      <input className="box" type="checkbox" name="React" onChange={(e) => checkboxHandler(e)} /><label> React</label><br />
-      <input className="box" type="checkbox" name="Redux" onChange={(e) => checkboxHandler(e)} /><label> Redux</label><br />
-      <input className="box" type="checkbox" name="Express" onChange={(e) => checkboxHandler(e)} /><label> Express</label><br />
+      <input className="box" type="checkbox" name="TypeScript" onChange={(e) => checkboxHandler(e)} /><label> TypeScript</label><br /><br />
+      <input className="box" type="checkbox" name="JS: Beginner" onChange={(e) => checkboxHandler(e)} /><label> JS: Beginner</label><br />
+      <input className="box" type="checkbox" name="JS: Intermediate" onChange={(e) => checkboxHandler(e)} /><label> JS: Intermediate</label><br />
+      <input className="box" type="checkbox" name="JS: Advanced" onChange={(e) => checkboxHandler(e)} /><label> JS: Advanced</label><br />
+      <input className="box" type="checkbox" name="Algorithms" onChange={(e) => checkboxHandler(e)} /><label> Algorithms</label><br />
       <input className="box" type="checkbox" name="Angular" onChange={(e) => checkboxHandler(e)} /><label> Angular</label><br />
       <input className="box" type="checkbox" name="Vue" onChange={(e) => checkboxHandler(e)} /><label> Vue</label><br />
+      <input className="box" type="checkbox" name="React" onChange={(e) => checkboxHandler(e)} /><label> React</label><br />
+      <input className="box" type="checkbox" name="Redux" onChange={(e) => checkboxHandler(e)} /><label> Redux</label><br />
+      <input className="box" type="checkbox" name="Node.JS" onChange={(e) => checkboxHandler(e)} /><label> Node.JS</label><br />
+      <input className="box" type="checkbox" name="Express" onChange={(e) => checkboxHandler(e)} /><label> Express</label><br />
       <input className="box" type="checkbox" name="Sequelize" onChange={(e) => checkboxHandler(e)} /><label> Sequelize</label><br />
-      <input className="box" type="checkbox" name="GraphQL" onChange={(e) => checkboxHandler(e)} /><label> GraphQL</label><br />
       <input className="box" type="checkbox" name="SQL" onChange={(e) => checkboxHandler(e)} /><label> SQL</label><br />
       <input className="box" type="checkbox" name="MongoDB" onChange={(e) => checkboxHandler(e)} /><label> MongoDB</label><br />
-      <input className="box" type="checkbox" name="JS: Advanced" onChange={(e) => checkboxHandler(e)} /><label> JS: Advanced</label><br />
-      <input className="box" type="checkbox" name="JS: Intermediate" onChange={(e) => checkboxHandler(e)} /><label> JS: Intermediate</label><br />
-      <input className="box" type="checkbox" name="JS: Beginner" onChange={(e) => checkboxHandler(e)} /><label> JS: Beginner</label><br />
-      <input className="box" type="checkbox" name="Algorithms" onChange={(e) => checkboxHandler(e)} /><label> Algorithms</label><br />
-      <input className="box" type="checkbox" name="TypeScript" onChange={(e) => checkboxHandler(e)} /><label> TypeScript</label><br /><br />
+      <input className="box" type="checkbox" name="GraphQL" onChange={(e) => checkboxHandler(e)} /><label> GraphQL</label><br />
       <button id="profileBtn" onClick={() => profileFetchHandler()}>Submit</button>
     </div>         
       )
